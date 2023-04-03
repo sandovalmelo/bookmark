b6775994054572925127b5c7d90333762c193478
Sandoval Melo <juniormelo777@gmail.com>
2023-04-03 19:58:18 -0300

    add features section styles and change tab scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// menu
 const menu = document.getElementById("menu");
 const openMenu = document.getElementById("open-menu");
 const closeMenu = document.getElementById("close-menu");
@@ -12,4 +13,31 @@ openMenu.addEventListener("click", () => {
 
 closeMenu.addEventListener("click", () => {
 	toggleMenu("false");
+});
+
+// tab
+const featuresOption = Array.from(document.querySelectorAll(".features-option"));
+const featuresTab = Array.from(document.querySelectorAll(".features-tab"));
+
+function changeTab(option) {
+	featuresTab.forEach((tab, index) => {
+		if (index === option) {
+			tab.setAttribute("data-open", "true");
+		} else {
+			tab.setAttribute("data-open", "false");
+		}
+	});
+}
+
+featuresOption.forEach((option, index) => {
+	option.addEventListener("click", (event) => {
+		featuresOption.filter((wrongOption) => {
+			if (wrongOption !== event.target) {
+				wrongOption.setAttribute("data-active", "false");
+			} else {
+				event.target.setAttribute("data-active", "true");
+				changeTab(+event.target.dataset.option);
+			}
+		});
+	});
 });
